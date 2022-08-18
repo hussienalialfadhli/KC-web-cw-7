704d34443fe67d00c55accf82ff5db4ebe565c36
hussienalialfadhli <109866037+hussienalialfadhli@users.noreply.github.com>
2022-08-18 03:20:55 +0300

    good

--- script.js
+++ script.js
@@ -1,1 +1,48 @@
 
+ 
+function BMI(weight, height) {
+
+let result = weight / (height * height)
+
+return result
+
+}
+
+
+console.log(BMI(70 , 1.7))
+
+function status(bmi) {
+if (bmi < 18.5) {
+return "‍لديك نقص في الوزن"
+
+}else if (bmi >= 18.5 && bmi < 25) {
+   return "وزنك صحي"
+
+}else{
+    return "لديك زيادة في الوزن"
+}
+
+
+}
+
+
+
+function calculate() {
+    let weight = document.getElementById("weight").value
+    let height = document.getElementById("height").value
+    let bmi = BMI(weight,height)
+    let desc = Status (bmi)
+    let div = document.getElementById("result")
+    div.innerText = bmi + " == " + desc
+}
+
+
+
+
+
+
+
+
+
+
+
